095193938161d86e4a3b1a1d8afff618fea367ed
mostafa12345 <hosseinzade2731@gmail.com>
2017-02-03 19:54:25 +0330

    new db

--- DB/news_smarty.sql
+++ DB/news_smarty.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 02, 2017 at 08:55 PM
+-- Generation Time: Feb 03, 2017 at 07:53 PM
 -- Server version: 5.5.53-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.20
 
@@ -19,6 +19,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `news_smarty`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `msg` varchar(3000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `phone` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `name`, `email`, `msg`, `created_at`, `phone`) VALUES
+(1, 'tesfdasdf', 'hosseinzade2731@gmail.com', 'asdasd', '2017-02-03 10:54:26', '2154'),
+(2, 'tesfdasdf', 'hosseinzade2731@gmail.com', 'asdasd', '2017-02-03 11:05:30', '2154');
 
 -- --------------------------------------------------------
 
@@ -52,21 +76,23 @@ CREATE TABLE IF NOT EXISTS `news` (
   `like_count` int(11) DEFAULT '0',
   `visit` int(11) DEFAULT '0',
   `cat_id` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `cat_id` (`cat_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `news`
 --
 
-INSERT INTO `news` (`id`, `title`, `short_note`, `note`, `auther_id`, `active`, `slider`, `pic_url`, `like_count`, `visit`, `cat_id`) VALUES
-(1, 'تست ۱', 'این یک تست است', 'این یک تست است', 1, 1, 0, 'test', 1, 1, 1),
-(2, 'تست ۱', 'این یک تست است خالی مینده', '<p>این یک تست است&nbsp;</p>\r\n', 1, 1, 1, NULL, 1, 1, 2),
-(4, 'TEst', 'test', '<p>test</p>\r\n', NULL, NULL, 1, 'photo_2016-10-12_16-31-22.jpg', 0, 0, 1),
-(5, 'Test add', 'test', '', NULL, NULL, 0, 'photo_2016-09-23_10-07-36.jpg', 0, 0, 2),
-(6, 'Test', 'test short note', '<p>hgg</p>\r\n', NULL, NULL, 0, 'photo_2016-09-23_10-07-36.jpg', 0, 0, 1),
-(8, 'Test', 'شسی', '<p>hgg</p>\r\n', NULL, NULL, 0, 'photo_2016-09-23_10-07-36.jpg', 0, 0, 3);
+INSERT INTO `news` (`id`, `title`, `short_note`, `note`, `auther_id`, `active`, `slider`, `pic_url`, `like_count`, `visit`, `cat_id`, `created_at`) VALUES
+(1, 'تست ۱', 'این یک تست است', 'این یک تست است', 1, 1, 0, NULL, 1, 1, 1, NULL),
+(2, 'تست ۱', 'این یک تست است خالی مینده', '<p>این یک تست است&nbsp;</p>\r\n', 1, 1, 1, 'man-back.jpg', 1, 1, 2, NULL),
+(4, 'TEst', 'این یک تست است که برای اسلایدر مورد استفاده قرار گرفته است', '<p>test</p>\r\n', NULL, NULL, 1, 'sparklers.jpg', 0, 0, 4, '2017-02-27 00:00:00'),
+(5, 'Test add', 'test', '', NULL, NULL, 0, 'photo_2016-09-23_10-07-36.jpg', 0, 0, 2, '2017-02-14 00:00:00'),
+(6, 'Test', 'test short note', '<p>hgg</p>\r\n', NULL, NULL, 0, 'photo_2016-09-23_10-07-36.jpg', 0, 0, 1, '2017-02-06 00:00:00'),
+(8, 'Test', 'شسی', '<p>hgg</p>\r\n', NULL, NULL, 0, 'photo_2016-09-23_10-07-36.jpg', 0, 0, 3, '2017-02-07 00:00:00'),
+(9, 'TEst', 'dssdf', '<p>sdfsdf</p>\r\n', NULL, NULL, 0, '', 0, 0, 6, '2017-02-03 09:37:30');
 
 -- --------------------------------------------------------
 
